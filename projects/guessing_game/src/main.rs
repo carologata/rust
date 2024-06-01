56c545f3c4142415c1737e6bbdd3272aa5bbf56a
Caroline Ogata <cogata@c3r11p5.42sp.org.br>
2024-06-01 11:17:29 -0300

    finished chapter 2

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -2,29 +2,45 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+
+/* The :: syntax in the ::new line indicates that new is an associated function of the String type. 
+An associated function is a function that’s implemented on a type, in this case String. This new function 
+creates a new, empty string. You’ll find a new function on many types because it’s a common name for a 
+function that makes a new value of some kind. */
+
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number)
+    loop
     {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number)
+        {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+    
 }
